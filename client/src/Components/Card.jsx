fa3818355903d1db5c6316ba9e35e519bd830dbe
Phonedolly <c1john0803@gmail.com>
2022-12-19 23:41:55 +0900

    adjust margin of Card

--- client/src/Components/Card.jsx
+++ client/src/Components/Card.jsx
@@ -3,16 +3,16 @@ import { motion } from "framer-motion";
 import Flex from "@react-css/flex";
 
 import Nothing from "../Images/nothing.jpg";
-import styles from "./Card.module.scss"
+import styles from "./Card.module.scss";
 
 export const Card = (props) => (
   <>
-    <motion.div className={styles.box} whileHover={{ scale: 1.02 }}>
-      {props.mode === "curator" ? (
+    {props.mode === "curator" ? (
+      <motion.div className={styles.box} whileHover={{ scale: 1.02 }}>
         <Flex column>
           <Link to={props.url}>
             <img
-              src={props.thumb ?? Nothing}
+              src={props.thumbnail || Nothing}
               alt="썸네일"
               className={styles.thumb}
             ></img>
@@ -20,7 +20,12 @@ export const Card = (props) => (
             <p className={styles.postDate}>{props.postDate}</p>
           </Link>
         </Flex>
-      ) : (
+      </motion.div>
+    ) : (
+      <motion.div
+        className={`${styles.box} ${styles.forArticle}`}
+        whileHover={{ scale: 1.02 }}
+      >
         <Flex column>
           <a href={props.ogLinkURL} target="_blank">
             {props.ogThumbnail ? (
@@ -34,10 +39,12 @@ export const Card = (props) => (
             )}
             <h2 className={styles.title}>{props.title}</h2>
             <p className={styles.summary}>{props.ogLinkSummary}</p>
-            <p className={styles.ogLinkRepresentativeUrl}>{props.ogLinkRepresentativeUrl}</p>
+            <p className={styles.ogLinkRepresentativeUrl}>
+              {props.ogLinkRepresentativeUrl}
+            </p>
           </a>
         </Flex>
-      )}
-    </motion.div>
+      </motion.div>
+    )}
   </>
 );
